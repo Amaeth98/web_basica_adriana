0237f76c63258d4b77e70e14a913bbde20350596
Amaeth <sniukis98@gmail.com>
2026-03-03 18:50:32 +0100

    Para docker

--- web_basica_adriana/src/main.tsx
+++ web_basica_adriana/src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import PokeApp from './PokeApp.tsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './pokeapi.css'
+import './App.css'
+import App from './App.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PokeApp />
+    <App />
   </StrictMode>
 );
